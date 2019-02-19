961c5685b8886258a93153528258b5419b4c703b
calube <caleb@byshowroom.com>
2019-02-18 19:36:38 -0800

    Initial Commit

---
+++ login-screen/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  login-screen
+//
+//  Created by Caleb Davis on 2/18/19.
+//  Copyright © 2019 Caleb Davis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
